4e32358449f085291ba3d582b4bd0209b675ca04
Mitchell Busby <mitchbusby@gmail.com>
2016-07-30 10:19:43 +1000

    Updates requirements and adds some nice scss stuff

--- govhack/__init__.py
+++ govhack/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask, url_for, redirect
 
+import assets
+
 app = Flask(__name__, instance_relative_config=True)
+
+
+assets.register_assets(app)
+
 
 import govhack.views
--- main.py
+++ main.py
@@ -12,4 +12,4 @@ from flask import Flask
 from govhack import app
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run('0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
