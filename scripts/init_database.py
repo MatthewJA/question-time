6b8231792a1603724ad3711f221ae6edd0873e30
MatthewJA <matthew.alger@gmail.com>
2016-07-31 11:01:06 +1000

    Initialised database

--- scripts/init_database.py
+++ scripts/init_database.py
@@ -1,29 +1,5 @@
 """Initialises the (temporary) SQLite3 database to dump location data into."""
 
-import sqlalchemy as sa
-import sqlalchemy.ext as ext
-import sqlalchemy.ext.declarative
-import sqlalchemy.orm as orm
+import database
 
-engine = sa.create_engine('sqlite:///hansard_processed.db')
-db_session = orm.scoped_session(
-        orm.sessionmaker(
-                autocommit=False,
-                autoflush=False,
-                bind=engine))
-Base = ext.declarative.declarative_base()
-Base.query = db_session.query_property()
-
-class DateHeat(Base):
-    __tablename__ = 'dateheat'
-    date = sa.Column(sa.Date(), primary_key=True)
-    heat = sa.Column(sa.String())
-
-    def __init__(self, date, heat):
-        self.date = date
-        self.heat = heat
-
-    def __repr__(self):
-        return '<DateHeat {}>'.format(self.date)
-
-Base.metadata.create_all(bind=engine)
+database.Base.metadata.create_all(bind=database.engine)
