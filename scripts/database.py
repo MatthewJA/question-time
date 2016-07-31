4a4d087871e27bf442a5e2e8229f533291cd3598
MatthewJA <matthew.alger@gmail.com>
2016-07-31 11:00:05 +1000

    Temp database

---
+++ scripts/database.py
@@ -0,0 +1,31 @@
+"""Models for the data processing scripts."""
+
+import sqlalchemy as sa
+import sqlalchemy.ext as ext
+import sqlalchemy.ext.declarative
+import sqlalchemy.orm as orm
+
+engine = sa.create_engine(os.environ[POSTGRES_URI])
+db_session = orm.scoped_session(
+        orm.sessionmaker(
+                autocommit=False,
+                autoflush=False,
+                bind=engine))
+Base = ext.declarative.declarative_base()
+Base.query = db_session.query_property()
+
+class DateHeat(Base):
+    __tablename__ = 'dateheat'
+    date = sa.Column(sa.Date(), primary_key=True)
+    heat = sa.Column(sa.String())
+    peaks = sa.Column(sa.String())
+    interest = sa.Column(sa.String())
+
+    def __init__(self, date, heat, peaks, interest):
+        self.date = date
+        self.heat = heat
+        self.peaks = peaks
+        self.interest = interest
+
+    def __repr__(self):
+        return '<DateHeat {}>'.format(self.date)
