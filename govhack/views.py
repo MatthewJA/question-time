2fc8573cafb1da912af235f481228ff0944ea151
Mitchell Busby <mitchbusby@gmail.com>
2016-09-17 08:53:25 +1000

    Prelim swagger documentation

--- govhack/views.py
+++ govhack/views.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import render_template, request
+from flask import render_template, request, abort
 
 import os
 import datetime
@@ -20,6 +20,20 @@ def index():
 
 @app.route('/interesting_trends/<place_name>')
 def interesting_trends(place_name):
+    """
+    Gets metadata about a point of interest for a particular date.
+    ---
+    description: ""
+    parameters:
+        - in: query
+          name: date
+        - in: path
+          name: place_name
+          type: string
+    responses:
+        '200':
+            description: Found POI for the given date.
+    """
     place_name = place_name.lower()
     date = request.args.get('date')
     date = datetime.datetime.strptime(date, '%Y-%m-%d')
@@ -46,13 +60,64 @@ def interesting_trends(place_name):
 
 @app.route('/points_of_interest')
 def points_of_interest():
+    """
+    Gets the points of interest for a particular date.
+    ---
+    description: ""
+    parameters:
+        - in: query
+          name: date
+          description: date that is to fetch the points of interest. Must be of the form YYYY-mm-dd
+          required: true
+          type: string
+    produces:
+        - application/json
+    responses:
+        '200':
+            description: Found points of interest.
+            schema:
+                id: PointsOfInterest
+                properties: 
+                    PointsOfInterest:
+                        type: array
+        '404':
+            description: Could not find points of interest for the given date.
+        '405':
+            description: Invalid input.
+    """
     date = request.args.get('date')
-    date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    try: 
+        date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    except:
+        abort(405)
     heatmap_points = models.DateHeat.query.filter_by(date=date).first()
+    if not heatmap_points:
+        abort(404)
     return '{"PointsOfInterest": %s}' % (heatmap_points.peaks)
 
 @app.route('/heatmap_points')
 def heatmap_points():
+    """
+    Gets a set of weighted heatmap latitude/longitude points.
+    ---
+    description: ""
+    parameters:
+        - in: query
+          name: date
+          type: string
+          description: date to be fetched.
+          required: true
+    produces:
+        - application/json
+    responses:
+        '200':
+            description: Found set of weighted heatmap points.
+            schema:
+                id: HeatmapPoints
+                properties:
+                    HeatmapPoints:
+                        type: array
+    """
     date = request.args.get('date')
     date = datetime.datetime.strptime(date, '%Y-%m-%d')
     heatmap_points = models.DateHeat.query.filter_by(date=date).first()
@@ -60,6 +125,22 @@ def heatmap_points():
 
 @app.route('/available_dates')
 def available_dates():
+    """
+    Gets a list of all available dates.
+    ---
+    description: ""
+    produces:
+        - application/json
+    responses:
+        '200':
+            description: Found dates.
+            schema:
+                id: HeatmapPoints
+                properties: 
+                    HeatmapPoints:
+                        type: array
+
+    """
     available_dates = [d.date for d in database.db_session.query(models.DateHeat.date).distinct()]
     return json.dumps({"AvailableDates":available_dates})
 
